6db05db43c965636d7d640e81b36e6a177b37253
Francisco Javier <fcojavier.garcia4@alu.uclm.es>
2019-11-14 01:01:23 +0100

    Creado JAR con Maven

---
+++ gestionTalleres/gestionTalleres_JAR/src/main/java/com/calatravaWare/ISO2/App.java
@@ -0,0 +1,13 @@
+package com.calatravaWare.ISO2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
